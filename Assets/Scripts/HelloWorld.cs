f4e6a44b853fdbd7830f91937b405f24cd190092
raven2290 <robertskody42@gmail.com>
2025-09-15 10:42:09 -0700

    unity project
    added unity project

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello world");
+        
+        Debug.Log("konnichiwa minasan");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
